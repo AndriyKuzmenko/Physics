1f59218e659bcb0658a9d665ab5bd37afeda4fc4
Andriy <ak8022@bs.amalnet.k12.il>
2021-10-18 16:23:46 +0300

    free fall data screen

--- app/src/main/java/com/example/physics/freeFallData.java
+++ app/src/main/java/com/example/physics/freeFallData.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class freeFallData extends AppCompatActivity {
-
+public class freeFallData extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_fall_data);
     }
