047422e2b68e7f98798803182b183382925f2b04
Andriy <ak8022@bs.amalnet.k12.il>
2021-10-18 14:13:09 +0300

    Languages

--- app/src/main/java/com/example/physics/Languages.java
+++ app/src/main/java/com/example/physics/Languages.java
@@ -1,4 +1,22 @@
 package com.example.physics;
 
-public class Languages {
+public class Languages
+{
+    public static String springElongation;
+    public static String pendulum;
+    public static String changeLanguage;
+
+    public void toEnglish()
+    {
+        springElongation="String Elongation";
+        pendulum="Pendulum";
+        changeLanguage="Change Language";
+    }
+
+    public void toHebrew()
+    {
+        springElongation="התארכות קפיץ";
+        pendulum="מטוטלת";
+        changeLanguage="שינוי שפה";
+    }
 }
