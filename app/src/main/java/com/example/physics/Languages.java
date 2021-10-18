e218450942df51a0f8a2bca273f312e410cb7372
Andriy <ak8022@bs.amalnet.k12.il>
2021-10-18 16:40:07 +0300

    free fall data screen

--- app/src/main/java/com/example/physics/Languages.java
+++ app/src/main/java/com/example/physics/Languages.java
@@ -6,6 +6,18 @@ public class Languages
     public static String pendulum;
     public static String credits;
     public static String freeFall;
+    public static String mass;
+    public static String height;
+    public static String planet;
+    public static String mercury;
+    public static String venus;
+    public static String earth;
+    public static String mars;
+    public static String jupiter;
+    public static String saturn;
+    public static String uranus;
+    public static String neptune;
+
 
     public static void toEnglish()
     {
@@ -13,6 +25,17 @@ public class Languages
         pendulum="Pendulum";
         credits="Credits";
         freeFall="Free Fall";
+        mass="Mass";
+        height="Height";
+        planet="Planet";
+        mercury="Mercury";
+        venus="Venus";
+        earth="Earth";
+        mars="Mars";
+        jupiter="Jupiter";
+        saturn="Saturn";
+        uranus="Uranus";
+        neptune="Neptune";
     }
 
     public static void toHebrew()
