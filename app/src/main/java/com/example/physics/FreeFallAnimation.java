68d32b55cec645c14b08b1dee85ed796cfac3c34
Andriy <ak8022@bs.amalnet.k12.il>
2021-10-20 12:56:33 +0300

    Animation

---
+++ app/src/main/java/com/example/physics/FreeFallAnimation.java
@@ -0,0 +1,14 @@
+package com.example.physics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreeFallAnimation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_fall_animation);
+    }
+}
